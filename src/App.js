61c9d5423657b8719a815e4351622f571af5bdd9
kkkkimtaehyeon <kth1927@naver.com>
2024-05-24 03:24:46 +0900

    main layout 제거

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Home from "./pages/Home";
 import YoungerOrder from "./pages/YoungerOrder";
 import UsingAI from "./pages/UsingAI";
 import NotFoundPage from "./pages/NotFoundPage";
-import MainLayout from "./layouts";
-import Search from "./pages/AI_search/Search";
 import SearchOrder from "./pages/AI_search/SearchOrder";
 import SearchV2 from "./pages/AI_search/Search_ver2";
 import Purchase from "./pages/Purchase";
@@ -16,66 +14,65 @@ import WebSocketTest from "./pages/test/WebSocketTest";
 import WebSocketTest2 from "./pages/test/WebSocketTest2";
 import WebSocketTest3 from "./pages/test/WebSocketTest3";
 import AudioStreaming from "./pages/test/AudioStreaming";
+import Tts from "./pages/test/Tts";
 
 function App() {
     const router = createBrowserRouter([
         {
             path: "/",
-            element: <MainLayout />,
-            children: [
-                {
-                    path: "/",
-                    element: <Home />,
-                },
-                {
-                    path: "search",
-                    element: <SearchV2 />
-                },
-                {
-                    path: "search-order",
-                    element: <SearchOrder />
-                },
-                // {
-                //     path: "olderorder",
-                //     element: <OlderOrder />,
-                // },
-                {
-                    path: "youngerorder",
-                    element: <YoungerOrder />,
-                },
-                {
-                    path: "usingai",
-                    element: <UsingAI />,
-                },
-                {
-                    path: "purchase",
-                    element: <Purchase />,
-                },
-                {
-                    path: "payment",
-                    element: <Payment />,
-                },
-                {
-                    path: "order-complete",
-                    element: <OrderComplete />,
-                },
-                {
-                    path: "test/websocket",
-                    element: <WebSocketTest />,
-                },
-                {
-                    path: "test/websocket/v2",
-                    element: <WebSocketTest2 />,
-                },
-                {
-                    path: "test/websocket/v3",
-                    element: <WebSocketTest3 />,
-                },
-                {
-                    path: "test/websocket/v4",
-                    element: <AudioStreaming />,
-                }
-            ],
+            element: <Home />,
+        },
+        {
+            path: "search",
+            element: <SearchV2 />
+        },
+        {
+            path: "search-order",
+            element: <SearchOrder />
+        },
+        // {
+        //     path: "olderorder",
+        //     element: <OlderOrder />,
+        // },
+        {
+            path: "youngerorder",
+            element: <YoungerOrder />,
+        },
+        {
+            path: "usingai",
+            element: <UsingAI />,
+        },
+        {
+            path: "purchase",
+            element: <Purchase />,
+        },
+        {
+            path: "payment",
+            element: <Payment />,
+        },
+        {
+            path: "order-complete",
+            element: <OrderComplete />,
+        },
+        {
+            path: "test/websocket",
+            element: <WebSocketTest />,
+        },
+        {
+            path: "test/websocket/v2",
+            element: <WebSocketTest2 />,
+        },
+        {
+            path: "test/websocket/v3",
+            element: <WebSocketTest3 />,
+        },
+        {
+            path: "test/websocket/v4",
+            element: <AudioStreaming />,
+        },
+        {
+            path: "test/tts",
+            element: <Tts />,
         },
         {
             path: "*",
