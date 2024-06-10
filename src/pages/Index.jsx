48e4812399c700a527d4c549bfd8efa1428d12ed
kkkkimtaehyeon <kth1927@naver.com>
2024-06-10 18:07:21 +0900

    리팩토링

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -6,7 +6,7 @@ export default function Index() {
 
     return (
         <Container>
-            <Link to="http://localhost:8080/manage" className="btn btn-lg btn-primary">관리</Link>
+            <Link to="https://responsivekiosk.store/manage" className="btn btn-lg btn-primary">관리</Link>
             <Link to="main" className="btn btn-lg btn-primary">시작</Link>
 
         </Container>
