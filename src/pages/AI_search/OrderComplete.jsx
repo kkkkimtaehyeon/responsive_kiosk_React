981f82576558f0f6ad43f054231c0efa4f1f7f51
Sinacs <111099679+GYHGwangju@users.noreply.github.com>
2024-06-12 18:47:25 +0900

    OrderComplete 완성

--- src/pages/AI_search/OrderComplete.jsx
+++ src/pages/AI_search/OrderComplete.jsx
@@ -7,16 +7,19 @@ const OrderComplete = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
-        setTimeout(() => {
-            navigate("/");
-        }, 10000)
-    }, []);
+        // Uncomment the below code to enable navigation after 10 seconds
+        // setTimeout(() => {
+        //     navigate("/");
+        // }, 10000)
+    }, [navigate]);
 
     return (
-        <Container>
-            <div>
-                주문완료! 주문번호는 <strong>{state}</strong>입니다!
+        <Container style={{fontFamily:'Nanum-Reg', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+            <div style={{textAlign:'center', margin:'45px',fontSize:'64pt'}}>
+                <p>주문 완료!</p>
+                <p style={{fontSize:'56pt'}}>주문번호는 <strong style={{color:'red', fontSize:'120pt'}}>{state}</strong>입니다!</p>
             </div>
+            <p style={{fontSize:'40pt', textAlign:'center', marginBottom: '10vh'}}>잠시 후 초기 화면으로 돌아갑니다...</p>
         </Container>
     );
 }
